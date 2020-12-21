e73b414877bb293d20599b5c225b322643a1e960
bleonahasanaj1 <75685284+bleonahasanaj1@users.noreply.github.com>
2020-12-21 01:08:22 +0100

    Counting Sort Algorithm
    Comparision Counting Sort and Distribution Counting Sort

---
+++ CountingSort/CountingSort.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CountingSort
+{
+    public class CountingSort
+    {
+        public int[] DistributionCountingSort(int[] array)
+        {
+            int[] sortedArray = new int[array.Length];
+
+            int minValue = Min(array);
+            int maxValue = Max(array);
+
+            int[] counts = new int[maxValue - minValue + 1];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                counts[array[i] - minValue]++;
+            }
+
+            //find distribution sum of counts array
+            counts[0]--;
+            for (int i = 1; i < counts.Length; i++)
+            {
+                counts[i] = counts[i] + counts[i - 1];
+            }
+
+            for (int i = array.Length - 1; i >= 0; i--)
+            {
+                sortedArray[counts[array[i] - minValue]] = array[i];
+                counts[array[i] - minValue]--;
+            }
+
+            return sortedArray;
+        }
+
+        public int[] ComparisionCountingSort(int[] array)
+        {
+            int[] sortedArray = new int[array.Length];
+
+            int minValue = Min(array);
+            int maxValue = Max(array);
+
+            int[] counts = new int[array.Length];
+
+            for (int i = 0; i < counts.Length - 1; i++)
+            {
+                for(int j = i+1; j < counts.Length; j++)
+                {
+                    if(array[i] < array[j])
+                    {
+                        counts[j]++;
+                    }
+                    else
+                    {
+                        counts[i]++;
+                    }
+                }
+            }
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                sortedArray[counts[i]] = array[i];
+            }
+
+            return sortedArray;
+        }
+
+        private int Min(int[] array)
+        {
+            int minimum = array[0];
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] < minimum)
+                {
+                    minimum = array[i];
+                }
+            }
+            return minimum;
+        }
+
+        private int Max(int[] array)
+        {
+            int maximum = array[0];
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] > maximum)
+                {
+                    maximum = array[i];
+                }
+            }
+
+            return maximum;
+        }
+    }
+}
---
+++ CountingSort/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+
+namespace CountingSort
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            CountingSort cs = new CountingSort();
+            Random randNum = new Random();
+            var watch = new System.Diagnostics.Stopwatch();
+            
+            //int[] array = { 2, 3, 4, 1, 1, 4, 5 };
+            int[] array = new int[1000000];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = randNum.Next(0, 1000);
+            }
+
+            Console.WriteLine("Distribution Counting Sort");
+
+            watch.Start();
+            int[] sortedArray1 = cs.DistributionCountingSort(array);
+
+            for (int i = 0; i < sortedArray1.Length; i++)
+            {
+                Console.Write(sortedArray1[i] + " ");
+            }
+            watch.Stop();
+            Console.WriteLine($"\n\nExecution Time of Distribution Sort: {watch.ElapsedMilliseconds} ms\n\n\n");
+
+
+            Console.WriteLine("Comparision Counting Sort");
+
+            watch.Start();
+            int[] sortedArray2 = cs.ComparisionCountingSort(array);
+
+            for (int i = 0; i < sortedArray2.Length; i++)
+            {
+                Console.Write(sortedArray2[i] + " ");
+            }
+            watch.Stop();
+            Console.WriteLine($"\n\nExecution Time of Comparision Sort: {watch.ElapsedMilliseconds} ms\n\n\n");
+        }
+
+    }
+}
